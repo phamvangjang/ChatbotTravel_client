f8e622bb465914b729e1e9d9063446e8ff9968b0
Pham Van Giang <104151063+phamvangjang@users.noreply.github.com>
2025-06-20 08:03:12 +0700

    feat: update otp

--- lib/models/otp_arguments_model.dart
+++
@@ -1,9 +0,0 @@
-import '../viewmodels/auth/verify_otp_viewmodel.dart';
-
-// class OtpArguments {
-//   final String email;
-//   final OtpType type;
-//   final String? resetToken;
-//
-//   OtpArguments({required this.email, required this.type, this.resetToken});
-// }
